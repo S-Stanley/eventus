a1db1bfe354c48df5d10c0401560e5ea26e39688
Stanley SERBIN <stanleyserbin@gmail.com>
2022-05-15 19:56:04 +0200

    feat: create POST /users

---
+++ services/api/routes/Users.ts
@@ -0,0 +1,37 @@
+export {};
+const express = require('express');
+
+const router = express.Router();
+
+import Helpers from "../database/helpers/Helpers";
+import Utils from '../utils/Utils'
+
+router.post('/', async(req, res) => {
+    try {
+        if (!Utils.Requests.verifParams(req.body, ['email', 'password', 'name', 'firstname', 'location'])){
+            res.status(422).json({
+                error: 'Missing parameter',
+            })
+        }
+        else {
+            const create = await Helpers.Users.create_users(
+                req.body['email'],
+                req.body['password'],
+                req.body['name'],
+                req.body['firstname'],
+                req.body['location'],
+            );
+            if (!create){
+                throw new Error("Cannot create user");
+            }
+            res.status(201).json(create);
+        }
+    } catch (e) {
+        console.log(e);
+        res.status(400).json({
+            error: e,
+        })
+    }
+});
+
+module.exports = router;
