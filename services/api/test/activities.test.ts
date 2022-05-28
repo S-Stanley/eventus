4c1ca6e0f093eb944a18f82e1e94e7bcc6828df9
Stanley SERBIN <stanleyserbin@gmail.com>
2022-05-29 01:39:54 +0200

    feat: add GET /activities/:activity_id/hosts

--- services/api/test/activities.test.ts
+++ services/api/test/activities.test.ts
@@ -23,23 +23,29 @@ describe('/activites', () => {
             res = JSON.parse(res.text);
             expect(res['name']).to.equal(name);
             expect(res['_id']).to.exist;
-        })
+        });
         it('return a HTTP 403', async function () {
             let res = await agent(server)
                 .post('/activities').send({
                     name: name,
                 });
             expect(res.status).to.equal(403);
-        })
+        });
         it('return a HTTP 422', async function () {
             let res = await agent(server).post('/activities')
             expect(res.status).to.equal(422);
-        })
+        });
     });
     describe("GET /activities", () => {
         it('Should return a 200', async () => {
             let req = await agent(server).get('/activities');
             expect(req.status).to.equal(200);
-        })
+        });
+    });
+    describe('GET /activities/:activity_id/hosts', () => {
+        it('should return a 422 wrong id format', async () => {
+            let req = await agent(server).get('/activities/wrongformatid/hosts');
+            expect(req.status).to.equal(422);
+        });
     });
 })
