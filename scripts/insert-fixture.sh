e8824656be2c64d0f11aee32cf9ee7a6c2f53253
Stanley SERBIN <stanleyserbin@gmail.com>
2022-09-25 15:25:41 +0200

    feat(fixtures): add specifique tests launcher for activites and events #139

--- scripts/insert-fixture.sh
+++ scripts/insert-fixture.sh
@@ -6,9 +6,9 @@ cd services/api/ || echo 'Error while trying to move to ios service folder';
 
 npm run build
 
-# node build/database/fixtures/Activities.js
-# node build/database/fixtures/Hosts.js
-# node build/database/fixtures/Events.js
+node build/database/fixtures/Activities.js
+node build/database/fixtures/Hosts.js
+node build/database/fixtures/Events.js
 node build/database/fixtures/fixtures.js
 
 cd -  || echo 'Fail to go back to previous pwd';
